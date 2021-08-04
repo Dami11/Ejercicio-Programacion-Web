629cea19e71cd580d371074743aee66571976656
ESI BUCEO <esi-buceo@outlook.com>
2021-08-04 18:47:10 -0300

    changed

---
+++ pedido.php
@@ -0,0 +1,20 @@
+<?php 
+
+class pedido{ 
+
+function montoTotal($detalle [], $i, $total){ 
+
+    if(!detalle [] && $i<=count($detalle[])){
+    $total+=total
+
+    montoTotal(+detalle[], $i++, $total);
+    
+    }
+
+}
+
+
+
+}
+
+?>
