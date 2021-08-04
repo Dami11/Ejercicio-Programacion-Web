3ff1b9fb0d987c5b5eaa72cebda86b979f32f233
ESI BUCEO <esi-buceo@outlook.com>
2021-08-04 19:09:38 -0300

    changed

--- pedido.php
+++ pedido.php
@@ -2,13 +2,38 @@
 
 class pedido{ 
 
+private $cantidad;
+private $precio_unitario;
+
+
+
+function calcularMonto(){ 
+
+
+    ($cant * $precio_unitario)
+    
+}    
+
+
+function getsubtotal(){ 
+
+
+    ($cant * $precio_unitario=$subtotal);
+
+    echo $this->cant;
+    
+
+}
+
+
+
 function montoTotal($detalle [], $i, $total){ 
 
     if(!detalle [] && $i<=count($detalle[])){
     $total+=total
 
     montoTotal(+detalle[], $i++, $total);
-    
+
     }
 
 }
